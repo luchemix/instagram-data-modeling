0cfda4cb2ffa7cea3f993db041143de0930601df
luchemix <luchemix@gmail.com>
2021-03-25 03:09:33 +0000

    initial

--- src/models.py
+++ src/models.py
@@ -8,23 +8,48 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Users(Base):
+    __tablename__ = 'users'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    profile_pic = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Post(Base):
+    __tablename__ = 'post'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user_pic = Column(String(250), ForeignKey('users.profile_pic'))
+    users = relationship(Users)
+    views = Column(Integer)
+    likes = Column(Integer)
+
+class History(Base):
+    __tablename__ = 'history'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    image = Column(String(250), nullable=False)
+    time = Column(Integer)
+    username = Column(String(250), ForeignKey('users.username'))
+    users = relationship(Users)
+
+class Feed(Base):
+    __tablename__ = 'feed'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post)
+    history_id = Column(Integer, ForeignKey('history.id'))
+    history = relationship(History)
+
 
     def to_dict(self):
         return {}
